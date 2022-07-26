d0f228415f798be1c6b3145eedee3b93a10feb05
Leonardo Marcarini Borges <leonardo.marcarini@yahoo.com.br>
2022-07-26 10:02:38 -0300

    feat(backArrow): Add logic and style

--- components/atoms/BackArrow/BackArrow.tsx
+++ components/atoms/BackArrow/BackArrow.tsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { useRouter } from "next/router";
+import styles from "./BackArrow.module.scss";
 
-type Props = {};
+const BackArrow = () => {
+  const router = useRouter();
 
-const BackArrow = (props: Props) => {
-  return <div>BackArrow</div>;
+  return (
+    <button
+      className={styles.backArrow}
+      type="button"
+      onClick={() => router.back()}
+    >
+      ←
+    </button>
+  );
 };
 
 export default BackArrow;
