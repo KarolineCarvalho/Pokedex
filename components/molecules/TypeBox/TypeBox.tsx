fdd78dfcfd539c0205d4ed8cb7be6f7a00b151c6
Leonardo Marcarini Borges <leonardo.marcarini@yahoo.com.br>
2022-07-26 16:15:05 -0300

    feat(TypeBox): Add type box component

---
+++ components/molecules/TypeBox/TypeBox.tsx
@@ -0,0 +1,20 @@
+import Text from "@atoms/Text";
+import PokemonType from "types/pokemonTypes";
+import styles from "./TypeBox.module.scss";
+
+type Props = {
+  type: PokemonType;
+};
+
+const TypeBox = ({ type }: Props): JSX.Element => {
+  return (
+    <div className={styles.typeBox}>
+      <div className={styles[`typeBox__background`]}></div>
+      <Text color="white" size="small" weight="normal">
+        {type}
+      </Text>
+    </div>
+  );
+};
+
+export default TypeBox;
