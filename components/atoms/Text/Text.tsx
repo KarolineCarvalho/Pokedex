40c5e79c7fb91ee1db67fae78b6831a2e49a2974
Karoline Carvalho <ka.olicarvalho@gmail.com>
2022-07-26 12:46:43 -0300

    Creating Text component

--- components/atoms/Text/Text.tsx
+++ components/atoms/Text/Text.tsx
@@ -1,9 +1,22 @@
+import styles from "./Text.module.scss";
+
 type Props = {
-  text: string;
+  children: React.ReactNode[] | React.ReactNode;
+  color: "black" | "grey" | "white";
+  size: "small" | "medium" | "large";
+  weight: "normal" | "bold";
 };
 
-const Text = ({ text }: Props): JSX.Element => {
-  return <p>{text}</p>;
+const Text = ({ children, color, size, weight }: Props): JSX.Element => {
+  const classList = [
+    styles[`text--${color}`],
+    styles[`text--${size}`],
+    styles[`text--${weight}`],
+  ];
+
+  const classes = classList.join(" ");
+
+  return <p className={classes}>{children}</p>;
 };
 
 export default Text;
--- pages/pokedex.tsx
+++ pages/pokedex.tsx
@@ -1,10 +1,14 @@
 import BackArrow from "@atoms/BackArrow";
+import Text from "@atoms/Text";
 import type { NextPage } from "next";
 
 const Pokedex: NextPage = () => {
   return (
     <div>
       <BackArrow />
+      <Text color="grey" size="large" weight="normal">
+        Teste
+      </Text>
     </div>
   );
 };
