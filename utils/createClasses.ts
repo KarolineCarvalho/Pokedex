7cca6bfc5a7ec67ed91438fedac8be95e8232ed4
Leonardo Marcarini Borges <leonardo.marcarini@yahoo.com.br>
2022-07-26 09:25:07 -0300

    feat: Creates a class handler utility

---
+++ utils/createClasses.ts
@@ -0,0 +1,26 @@
+const createClasses = (
+  base: string,
+  styles: {
+    readonly [key: string]: string;
+  },
+  modifiers: string[] = []
+) => {
+  const classes = [
+    styles[base],
+    ...modifiers.map((modifier) => styles[base + "--" + modifier]),
+  ];
+
+  const addClass = (name: string) => {
+    classes.push(styles[base + "--" + name]);
+    return createClasses(base, styles, [...modifiers, name]);
+  };
+
+  const getClasses = () => classes.join(" ");
+
+  return {
+    addClass,
+    getClasses,
+  };
+};
+
+export default createClasses;
