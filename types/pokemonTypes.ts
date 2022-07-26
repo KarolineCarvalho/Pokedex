771ed0d40b6500a4a964081eee8cf358163bdd17
Leonardo Marcarini Borges <leonardo.marcarini@yahoo.com.br>
2022-07-26 15:52:28 -0300

    feat: Add pkm types

---
+++ types/pokemonTypes.ts
@@ -0,0 +1,21 @@
+type PokemonType =
+  | "normal"
+  | "fight"
+  | "flying"
+  | "poison"
+  | "ground"
+  | "rock"
+  | "bug"
+  | "ghost"
+  | "steel"
+  | "fire"
+  | "water"
+  | "grass"
+  | "electr"
+  | "psychc"
+  | "ice"
+  | "dragon"
+  | "dark"
+  | "fairy";
+
+export default PokemonType;
