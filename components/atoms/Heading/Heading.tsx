9008476bb84fbdece21314d2d5a4f2c95eb86e93
Leonardo Marcarini Borges <leonardo.marcarini@yahoo.com.br>
2022-07-26 09:28:13 -0300

    feat(heading): Add white color modifier

--- components/atoms/Heading/Heading.tsx
+++ components/atoms/Heading/Heading.tsx
@@ -1,12 +1,21 @@
+import createClasses from "@utils/createClasses";
 import React from "react";
+import styles from "./Heading.module.scss";
 
 type Props = {
   level?: number;
   children: React.ReactNode[] | React.ReactNode;
+  white?: boolean;
 };
 
-const Heading = ({ children, level = 1 }: Props): JSX.Element => {
-  return React.createElement("h" + level, {}, children);
+const Heading = ({ children, level = 1, white }: Props): JSX.Element => {
+  const classes = createClasses("heading", styles);
+  if (white) classes.addClass("white");
+  return React.createElement(
+    "h" + level,
+    { className: classes.getClasses() },
+    children
+  );
 };
 
 export default Heading;
