23d6459ed0a59327d436f5bc7cb84124ac35c1d9
Karoline Carvalho <ka.olicarvalho@gmail.com>
2022-07-29 12:54:07 -0300

    Creating square component

---
+++ components/atoms/SquareIcon/SquareIcon.tsx
@@ -0,0 +1,37 @@
+import createClasses from "@utils/createClasses";
+import styles from "./SquareIcon.module.scss";
+
+type Props = {
+  color?: string;
+  opacity?: "normal" | "high";
+};
+
+const SquareIcon = ({ color, opacity }: Props): JSX.Element => {
+  const classes = createClasses("SquareIcon", styles);
+  if (opacity) classes.addClass("opacity--" + opacity);
+  return (
+    <div className={styles.SquareIcon}>
+      <svg
+        width="166"
+        height="166"
+        viewBox="0 0 166 166"
+        fill={color ? color : "white"}
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g className={classes.getClasses()}>
+          <rect
+            x="134.02"
+            y="-5"
+            width="143.924"
+            height="143.924"
+            rx="24"
+            transform="rotate(75 134.02 -5)"
+            fill={color ? color : "white"}
+          />
+        </g>
+      </svg>
+    </div>
+  );
+};
+
+export default SquareIcon;
