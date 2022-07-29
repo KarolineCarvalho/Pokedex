41915220d84b032b9459f5f8fd1a63ec2ff9761d
Leonardo Marcarini Borges <leonardo.marcarini@yahoo.com.br>
2022-07-29 08:50:40 -0300

    Refactor(pokecard): Add an conversion to string

--- components/molecules/PokeCard/PokeCard.tsx
+++ components/molecules/PokeCard/PokeCard.tsx
@@ -3,7 +3,6 @@ import Text from "@atoms/Text";
 import Image from "@molecules/Image";
 import TypeBox from "@molecules/TypeBox";
 import createClasses from "@utils/createClasses";
-import { type } from "os";
 import Pokemon from "types/Pokemon";
 import styles from "./PokeCard.module.scss";
 
@@ -12,14 +11,16 @@ type Props = {
 };
 
 const PokeCard = ({ pokemon }: Props) => {
+  console.log(pokemon);
   const { id, name, types, sprite } = pokemon;
+  console.log(`PokeCard: ${types}`);
   const cardClasses = createClasses("pokeCard", styles);
   if (types.length > 0) cardClasses.addClass("type--" + types[0]);
   return (
     <div className={cardClasses.getClasses()}>
       <div className={cardClasses.getElement("id")}>
         <Text color="white" size="medium" weight="normal">
-          #{id.padStart(3, "0")}
+          #{id.toString().padStart(3, "0")}
         </Text>
       </div>
       <div className={cardClasses.getElement("name")}>
