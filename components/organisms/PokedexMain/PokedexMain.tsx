0cfd38814bbfe1c2ac1de0c2f995811cdfbb6f72
Leonardo Marcarini Borges <leonardo.marcarini@yahoo.com.br>
2022-07-29 11:10:48 -0300

    feat(pokedexpage): Add filter to pokemon list

--- components/organisms/PokedexMain/PokedexMain.tsx
+++ components/organisms/PokedexMain/PokedexMain.tsx
@@ -4,19 +4,36 @@ import PokeCard from "@molecules/PokeCard";
 import usePokemon from "hooks/usePokemon";
 import Link from "next/link";
 import styles from "./PokedexMain.module.scss";
+import { useRouter } from "next/router";
+import { useMemo } from "react";
+import { filterPokemon } from "./utils/filterPokemon";
+import SkeletonBox from "@atoms/SkeletonBox";
 
 const PokedexMain = () => {
+  const router = useRouter();
+  const { search } = router.query;
   const { pokemon, isLoading, isError } = usePokemon();
+
+  const pokemonFiltered = useMemo(
+    () => filterPokemon(search, pokemon),
+    [pokemon, search]
+  );
 
   return (
     <main>
       <section aria-label="pokemon list" className={styles["pokedexSection"]}>
         <Heading>Pokedex</Heading>
+        {isLoading && (
+          <Grid type="pokedex">
+            {isLoading &&
+              new Array(6).fill("").map((_, i) => <SkeletonBox key={i} />)}
+          </Grid>
+        )}
         <Grid type="pokedex">
           {!isLoading &&
             !isError &&
-            pokemon &&
-            pokemon.map((singlePokemon) => (
+            pokemonFiltered &&
+            pokemonFiltered.map((singlePokemon) => (
               <Link
                 href={`/pokemon/${singlePokemon.id}`}
                 key={singlePokemon.id}
---
+++ components/organisms/PokedexMain/utils/filterPokemon.ts
@@ -0,0 +1,18 @@
+import Pokemon from "types/Pokemon";
+
+export const filterPokemon = (
+  search: string | string[] | undefined,
+  pokemon: Pokemon[] | undefined
+): Pokemon[] | undefined => {
+  if (!pokemon) return undefined;
+  if (!search) return pokemon;
+  let searchString = (Array.isArray(search) ? search.join(" ") : search).trim();
+  return pokemon.filter((pk) =>
+    JSON.stringify([
+      pk.name,
+      pk.types,
+      pk.abilities.map((ab) => ab.ability.name),
+      pk.moves.map((mv) => mv.move.name),
+    ]).includes(searchString)
+  );
+};
