71aa81217e6d187eadc546e690d73debacc9f322
Leonardo Marcarini Borges <leonardo.marcarini@yahoo.com.br>
2022-07-29 08:53:07 -0300

    refactor: Change poekmon type

--- types/Pokemon.ts
+++ types/Pokemon.ts
@@ -1,10 +1,12 @@
 import PokemonType from "./PokemonTypes";
 
 type Pokemon = {
-  id: string;
+  id: number;
   name: string;
   types: PokemonType[];
   sprite: string;
+  sprites: { front_default: string };
+  url: string;
 };
 
 export default Pokemon;
