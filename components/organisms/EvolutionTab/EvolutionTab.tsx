6d362b6f1b1deb8df764f3449d7f8b873eba6bc8
Leonardo Marcarini Borges <leonardo.marcarini@yahoo.com.br>
2022-08-03 10:10:45 -0300

    feat(evolutionTab): Add heading and css

--- components/organisms/EvolutionTab/EvolutionTab.tsx
+++ components/organisms/EvolutionTab/EvolutionTab.tsx
@@ -1,3 +1,4 @@
+import Heading from "@atoms/Heading";
 import EvolutionChain from "@molecules/EvolutionChain";
 import useSinglePokemon from "hooks/useSinglePokemon";
 import styles from "./EvolutionTab.module.scss";
@@ -16,7 +17,8 @@ const EvolutionTab = ({ currentPokemon }: Props) => {
   );
 
   return (
-    <div>
+    <div className={styles.evolutionTab}>
+      <Heading level={3}>Evolution Chain</Heading>
       {evolutionchain?.chain && <EvolutionChain chain={evolutionchain.chain} />}
     </div>
   );
