be9edeb269f81ef49c0d70f8c7de555354881285
Leonardo Marcarini Borges <leonardo.marcarini@yahoo.com.br>
2022-08-03 09:39:12 -0300

    feat(evolutionchain) : Show one layer of the evolution chain

--- components/organisms/EvolutionTab/EvolutionTab.tsx
+++ components/organisms/EvolutionTab/EvolutionTab.tsx
@@ -1,11 +1,29 @@
+import useSinglePokemon from "hooks/useSinglePokemon";
 import styles from "./EvolutionTab.module.scss";
 
 type Props = {
   currentPokemon: string;
 };
 
-const EvolutionTab = (props: Props) => {
-  return <div>EvolutionTab</div>;
+const EvolutionTab = ({ currentPokemon }: Props) => {
+  const { pokemon } = useSinglePokemon(`pokemon-species/${currentPokemon}`);
+
+  const chainID = pokemon?.evolution_chain?.url?.match(/\d+\/$/);
+  const { pokemon: evolutionchain } = useSinglePokemon(
+    chainID ? `evolution-chain/${chainID[0]}` : "",
+    !!pokemon
+  );
+
+  return (
+    <div>
+      {evolutionchain?.chain?.evolves_to &&
+        evolutionchain.chain.evolves_to.map((evolution: any, i: number) => (
+          <p key={i}>
+            {evolutionchain.chain.species.name} {"->"} {evolution.species.name}
+          </p>
+        ))}
+    </div>
+  );
 };
 
 export default EvolutionTab;
